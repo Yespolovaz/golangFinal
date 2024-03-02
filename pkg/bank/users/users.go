9d19ba1a487f116436609a68328957a4e3149761
Yespolovaz <z_espolova@kbtu.kz>
2024-03-02 11:31:23 +0600

    logging in

--- pkg/bank/users/users.go
+++ pkg/bank/users/users.go
@@ -1,1 +1,51 @@
 package users
+
+import (
+	"time"
+
+	"github.com/Yespolovaz/golangFinal/pkg/bank/helpers"
+	"github.com/Yespolovaz/golangFinal/pkg/bank/interfaces"
+	"github.com/Yespolovaz/golangFinal/pkg/bank/migrations"
+	"github.com/dgrijalva/jwt-go"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func Login(username string, pass string) map[string]interface{} {
+	db := helpers.ConnectDB()
+	user := &migrations.User{}
+	if db.Where("username = ? ", username).First(&user).RecordNotFound() {
+		return map[string]interface{}{"message": "User not found"}
+	}
+
+	passErr := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(pass))
+	if passErr == bcrypt.ErrMismatchedHashAndPassword && passErr != nil {
+		return map[string]interface{}{"message": "Wrong password"}
+	}
+
+	accounts := []interfaces.ResponseAccount{}
+	db.Table("accounts").Select("id, name, balance").Where("user_id = ? ", user.ID).Scan(&accounts)
+
+	responseUser := &interfaces.ResponseUser{
+		ID:       user.ID,
+		Username: user.Username,
+		Email:    user.Email,
+		Accounts: accounts,
+	}
+
+	defer db.Close()
+
+	tokenContent := jwt.MapClaims{
+		"user_id": user.ID,
+		"expiry":  time.Now().Add(time.Minute * 60).Unix(),
+	}
+	jwtToken := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), tokenContent)
+	token, err := jwtToken.SignedString([]byte("TokenPassword"))
+	helpers.HandleErr(err)
+
+	var response = map[string]interface{}{"message": "all is fine"}
+	response["jwt"] = token
+	response["data"] = responseUser
+
+	return response
+
+}
