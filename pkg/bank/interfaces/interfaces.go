10125b44e798c105310524467b44791c0e7479d4
Yespolovaz <z_espolova@kbtu.kz>
2024-03-02 11:30:33 +0600

    interface for account response

--- pkg/bank/interfaces/interfaces.go
+++ pkg/bank/interfaces/interfaces.go
@@ -1,1 +1,14 @@
 package interfaces
+
+type ResponseAccount struct {
+    ID uint
+    Name string
+    Balance int
+}
+
+type ResponseUser struct {
+    ID uint
+    Username string
+    Email string
+    Accounts []ResponseAccount
+}
