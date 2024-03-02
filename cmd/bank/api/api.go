a1ebc18752aa691d2786911a7b5228a3a9f2876a
Yespolovaz <z_espolova@kbtu.kz>
2024-03-02 11:30:58 +0600

    login function

--- cmd/bank/api/api.go
+++ cmd/bank/api/api.go
@@ -1,1 +1,56 @@
 package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/Yespolovaz/golangFinal/pkg/bank/helpers"
+	"github.com/Yespolovaz/golangFinal/pkg/bank/users"
+
+	"github.com/gorilla/mux"
+)
+
+type Login struct {
+	Username string
+	Password string
+}
+
+type ErrResponse struct {
+	Message string
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	helpers.HandleErr(err)
+
+	var formattedBody Login
+	err = json.Unmarshal(body, &formattedBody)
+	helpers.HandleErr(err)
+	login := users.Login(formattedBody.Username, formattedBody.Password)
+
+	if login["message"] == "all is fine" {
+		resp := login
+		json.NewEncoder(w).Encode(resp)
+	} else {
+
+	}
+
+	if login["message"] == "all is fine" {
+		resp := login
+		json.NewEncoder(w).Encode(resp)
+	} else {
+		resp := ErrResponse{Message: "Wrong username or password"}
+		json.NewEncoder(w).Encode(resp)
+	}
+}
+
+func StartApi() {
+	router := mux.NewRouter()
+	router.HandleFunc("/login", login).Methods("POST")
+	fmt.Println("App is working on port :8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
+
+}
